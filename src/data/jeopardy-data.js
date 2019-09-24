8816e94262496a7cbe9450cab3d8e5b0cbc9905e
John Serrao <bigbadserrao@gmail.com>
2019-09-24 16:43:02 -0400

    Moved data to a separate file

---
+++ src/data/jeopardy-data.js
@@ -0,0 +1,22 @@
+// This is just an object, don't forget to export it
+const jeopardyData = {
+  "id": 70482,
+  "answer": "golf",
+  "question": "In the early 20th century, Arthur Groom introduced this sport to Japan & boy, were they teed off",
+  "value": 400,
+  "airdate": "2006-06-14T12:00:00.000Z",
+  "created_at": "2014-02-11T23:31:41.369Z",
+  "updated_at": "2014-02-11T23:31:41.369Z",
+  "category_id": 5829,
+  "game_id": null,
+  "invalid_count": null,
+  "category": {
+    "id": 5829,
+    "title": "made in japan",
+    "created_at": "2014-02-11T23:14:16.327Z",
+    "updated_at": "2014-02-11T23:14:16.327Z",
+    "clues_count": 10
+  }
+}
+
+export default jeopardyData
